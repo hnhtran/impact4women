fb645af7d0bd424eebf331467b90e0fbda7a577b
hnhtran <tran_eetas@yahoo.com>
2022-10-22 11:14:33 +1100

    add supplier init

---
+++ src/components/AddRecipient/AddRecipient.jsx
@@ -0,0 +1,7 @@
+import './AddRecipient.css'
+
+export const AddRecipient = () => {
+  return (
+    <div>AddRecipient</div>
+  )
+}
--- src/components/AddRecipients/AddRecipients.jsx
+++
@@ -1,7 +0,0 @@
-import './AddRecipients.css'
-
-export const AddRecipients = () => {
-  return (
-    <div>AddRecipients</div>
-  )
-}
