626d2b249893b531965c3f6ad525612d3f230fac
hnhtran <tran_eetas@yahoo.com>
2022-10-22 13:35:07 +1100

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,10 @@ import { Navbar } from 'reactstrap'
 
 export const NavBar = () => {
   return (
-    <Navbar>
+    <Navbar className='nav'>
         <img src="impactLogo.png" alt="impact logo" className='Logo'/>
         
+        <img src='usericonOG.png' className="profilePicture" alt=""/>
     </Navbar>
   )
 }
